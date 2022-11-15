41761c69a1b22676299f3f86cf37aa6f3da0890e
Vytenis <vytenis.kondrackis@gmail.com>
2022-11-15 16:40:59 +0200

    added fetch project, changed few links on projects

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -14,36 +14,49 @@ const Projects = () => {
         </h1>
 
         <div className='max-w-7xl mx-auto grid sm:grid-cols-2 md:grid-cols-3 gap-8'>
-          {projects.map(({ id, title, imageSrc, url, style, text }) => (
-            <div key={id}>
-              <div className=' group shadow-md shadow-gray-600 bg-transparent overflow-hidden rounded-md border-white'>
-                <h2 className='text-center text-2xl text-white capitalize my-4 font-bold duration-200 group-hover:underline underline-offset-4'>
-                  {title}
-                </h2>
-                <div className='rounded-md duration-200 group-hover:scale-110 flex'>
-                  <Image src={imageSrc} alt={title} />
-                </div>
+          {projects.map(
+            ({ id, title, imageSrc, github, live, style, styleTwo, text }) => (
+              <div key={id}>
+                <div className=' group shadow-md shadow-gray-600 bg-transparent overflow-hidden rounded-md border-white'>
+                  <h2 className='text-center text-2xl text-white capitalize my-4 font-bold duration-200 group-hover:underline underline-offset-4'>
+                    {title}
+                  </h2>
+                  <div className='rounded-md duration-200 group-hover:scale-110 flex'>
+                    <Image src={imageSrc} alt={title} />
+                  </div>
 
-                <div className='flex items-center justify-center capitalize font-bold my-3'>
-                  <div className='font-light text-base text-white'>
-                    Technologies used for this project:<br></br>
-                    <p className='font-bold'>{text}</p>
+                  <div className='flex items-center justify-center capitalize font-bold my-3'>
+                    <div className='font-light text-base text-white'>
+                      Technologies used for this project:<br></br>
+                      <p className='font-bold'>{text}</p>
+                    </div>
+                  </div>
+                  <div className='flex flex-col'>
+                    <a
+                      className={`flex items-center justify-center ${style}`}
+                      target='_blank'
+                      rel='noreferrer'
+                      href={github}
+                    >
+                      <button className='w-1/2 px-6 py-2 m-2 duration-200 text-yellow-400 bg-transparent font-bold border-yellow-400 hover:scale-105 hover:text-white hover:border-white'>
+                        Code
+                      </button>
+                    </a>
+                    <a
+                      className={`flex items-center justify-center ${styleTwo}`}
+                      target='_blank'
+                      rel='noreferrer'
+                      href={live}
+                    >
+                      <button className='w-1/2 px-6 py-2 m-2 duration-200 text-yellow-400 bg-transparent font-bold border-yellow-400 hover:scale-105 hover:text-white hover:border-white'>
+                        Live
+                      </button>
+                    </a>
                   </div>
                 </div>
-
-                <a
-                  className={`flex items-center justify-center ${style}`}
-                  target='_blank'
-                  rel='noreferrer'
-                  href='https://github.com/Vytenis1337/vytenis-portfolio'
-                >
-                  <button className='w-1/2 px-6 py-2 m-2 duration-200 text-yellow-400 bg-transparent font-bold border-yellow-400 hover:scale-105 hover:text-white hover:border-white'>
-                    Code
-                  </button>
-                </a>
               </div>
-            </div>
-          ))}
+            )
+          )}
         </div>
       </div>
     </div>
--- services/projectServices.jsx
+++ services/projectServices.jsx
@@ -1,21 +1,24 @@
-import projectPort from '../public/assets/projectPort.jpg';
 import comingSoon from '../public/assets/projects/comingSoonPexel.jpg';
+import fetchApi from '../public/assets/projects/FetchAPI_project.png';
+import portfolioProject from '../public/assets/projects/PortfolioProject.png';
 
 export const projects = [
   {
     id: 1,
     title: 'portfolio website',
-    imageSrc: projectPort,
-    // url: 'vytenis-portfolio',
+    imageSrc: portfolioProject,
+    github: 'https://github.com/Vytenis1337/vytenis-portfolio',
     text: ' Javascript NextJs Tailwind Framer-Motion',
+    styleTwo: 'hidden',
   },
   {
     id: 2,
     title: 'fetch API',
-    imageSrc: comingSoon,
-    // url: 'fetch-api',
-    style: 'hidden',
-    text: ' Javascript ReactJs Typescript Sass',
+    imageSrc: fetchApi,
+    github: 'https://github.com/Vytenis1337/fetch-api/tree/master',
+    live: 'https://vytenis-fetch-api.netlify.app/',
+
+    text: ' Javascript ReactJs Tailwind Axios ContextAPI',
   },
   {
     id: 3,
@@ -24,5 +27,6 @@ export const projects = [
     // url: 'advanced-calculator',
     style: 'hidden',
     text: 'Javascript NextJs Redux Tailwind',
+    styleTwo: 'hidden',
   },
 ];
