11f8cfd0a1af78b5bc0cf727c8264b3afcf13d5f
Vytenis <vytenis.kondrackis@gmail.com>
2023-02-02 15:38:07 +0200

    added users-list project

--- services/projectServices.jsx
+++ services/projectServices.jsx
@@ -2,6 +2,7 @@ import todolist from '../public/assets/projects/TodosProject.png';
 import fetchApi from '../public/assets/projects/FetchProject.jpg';
 import portfolioProject from '../public/assets/projects/PortProject.png';
 import ecommerce from '../public/assets/projects/EcommerceProject.png';
+import usersList from '../public/assets/projects/UsersListProject.png';
 
 export const projects = [
   {
@@ -12,8 +13,17 @@ export const projects = [
     text: ' Javascript ReactJs TypeScript CSS Framer-Motion Axios ContextAPI',
     live: 'https://shop-app.netlify.app/',
   },
+
   {
     id: 2,
+    title: 'users list',
+    imageSrc: usersList,
+    github: 'https://github.com/Vytenis1337/user-list/tree/master',
+    text: ' Javascript ReactJs CSS Framer-Motion Axios ContextAPI',
+    live: 'https://user-list-add-edit.netlify.app',
+  },
+  {
+    id: 3,
     title: 'portfolio website',
     imageSrc: portfolioProject,
     github: 'https://github.com/Vytenis1337/vytenis-portfolio',
@@ -21,7 +31,7 @@ export const projects = [
     styleTwo: 'hidden',
   },
   {
-    id: 3,
+    id: 4,
     title: 'fetch API',
     imageSrc: fetchApi,
     github: 'https://github.com/Vytenis1337/fetch-api/tree/master',
@@ -30,7 +40,7 @@ export const projects = [
     text: ' Javascript ReactJs Tailwind Axios ContextAPI',
   },
   {
-    id: 4,
+    id: 5,
     title: 'todo list',
     imageSrc: todolist,
     github: 'https://github.com/Vytenis1337/todo-app',
