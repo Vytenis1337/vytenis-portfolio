04350adc93a133a130397cc7abb4a481d2b89021
Vytenis <vytenis.kondrackis@gmail.com>
2024-04-24 14:52:29 +0300

    changed Skills Images

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,31 +3,24 @@ import Image from "next/image";
 
 const Skills = () => {
   return (
-    <div id="skills" className="w-full h-full lg:h-screen bg-transparent  ">
-      <div className="h-full mx-auto px-8  text-center max-w-[1240px]">
-        <h1 className="font-sans text-5xl md:text-6xl lg:text-7xl font-bold text-white mb-4 ">
+    <div id="skills" className="w-full h-full lg:h-screen  ">
+      <div className="h-full mx-auto px-8 md:py-16 text-center max-w-[1240px]">
+        <h1 className="font-mono text-5xl md:text-6xl font-bold text-white ">
           Skills
         </h1>
 
         <div
           data-aos="fade-left"
-          className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:pt-10 gap-5 mt-5"
+          className="grid grid-cols-2 md:grid-cols-3 lg:pt-10 gap-2 md:gap-3 lg:gap-5 mt-5"
         >
           {skills.map(({ id, title, src, color }) => (
             <div
               key={id}
-              className="group flex flex-col items-center justify-center p-3  hover:border-white border-2 border-transparent rounded-lg"
+              whileHover={{ scale: 1.1 }}
+              className={`group flex flex-col  gap-2 lg:gap-0 items-center justify-center p-2 lg:p-3 hover:border-none ease-in duration-300 border-2  border-blue-400 group-hover:scale-105 bg-white`}
             >
-              <Image
-                src={src}
-                width="55px"
-                height="55px"
-                alt={title}
-                className={`transition duration-300 ${
-                  color === "white" ? "bg-white" : "bg-transparent"
-                } ease-in-out transform group-hover:scale-105`}
-              />
-              <h3 className="font-bold text-md opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out text-white">
+              <Image src={src} width="55px" height="55px" alt={title} />
+              <h3 className="mx-1 font-bold text-black opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
                 {title}
               </h3>
             </div>
