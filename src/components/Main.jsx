ee07c65b7fe21508fe5f07df9dab1908ad66b5bb
Vytenis <vytenis.kondrackis@gmail.com>
2024-05-06 19:37:54 +0300

    added apos to Main component middle section and changed position of buttons to make them visible on smaller screens

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,7 +5,7 @@ const Main = () => {
   return (
     <div id="home" className="bg-cover bg-center w-full h-screen relative">
       <div className="absolute inset-0 w-full h-full flex">
-        <div className="w-1/2 h-full flex items-center justify-center bg-black/30 transition-colors duration-300 ease-in-out group hover:bg-black/50">
+        <div className="w-1/2 h-full flex items-end pb-40 xl:pb-0 xl:items-center justify-center bg-black/30 transition-colors duration-300 ease-in-out group hover:bg-black/50">
           <div className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-center">
             <Link href="#contact" scroll={false}>
               <button className="text-lg font-bold text-white py-2 px-4 bg-blue-500 hover:bg-blue-700 rounded">
@@ -15,11 +15,11 @@ const Main = () => {
           </div>
         </div>
 
-        <div className="w-1/2 h-full flex items-center justify-center bg-black/30 transition-colors duration-300 ease-in-out  group hover:bg-black/50">
+        <div className="w-1/2 h-full flex items-end xl:items-center pb-40 xl:pb-0 justify-center bg-black/30 transition-colors duration-300 ease-in-out  group hover:bg-black/50">
           <div className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-center">
             <Link href="#projects" scroll={false}>
               <button className="text-lg font-bold text-white py-2 px-4 bg-teal-500 hover:bg-teal-700 rounded">
-                View My Projects
+                My Projects
               </button>
             </Link>
           </div>
@@ -29,7 +29,7 @@ const Main = () => {
       <div className="absolute w-full h-full flex items-center justify-center pointer-events-none">
         <div className="text-center p-4 backdrop-blur-md bg-black/30 rounded-lg shadow-lg">
           <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-white leading-tight shadow-lg">
-            Hi, Im{" "}
+            Hi, I&apos;m{" "}
             <span className="text-blue-400 cursor-pointer bg-white/20 px-2 py-1 rounded hover:text-teal-400 transition-colors duration-300 pointer-events-auto">
               <Link href="#about" scroll={false}>
                 &lt;Vytenis&gt;
